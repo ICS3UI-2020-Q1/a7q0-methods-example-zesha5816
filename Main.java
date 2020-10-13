c96551ec15f2e46db6c725c07e4a5abe51ede245
zesha5816 <zesha5816@wrdsb.ca>
2020-10-13 12:51:44 +0000

    finished example

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * method example file
+ * @author Ahmad Zeshan
  */
 public class Main {
+	
+	public static void sayhello(){
+		System.out.println("Hello World!");
+	} 
+	public static void sayhello(String name){
+		System.out.println("Hello " + name);
+	}
+	//a method to calculate the area of a rectangle
+	public static double areaOfRectangle(double width, double length){
+		double area = width * length;
+		return area;
+	}
 
   /**
    * The method that is executed when you hit the run button.
@@ -10,6 +23,12 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+		 Scanner input = new Scanner(System.in);
+		 double answer = areaOfRectangle(2.0, 5.0);
+		 System.out.println(answer);
+
+		 //print hello World
+		 sayhello();
+		 sayhello("Ahmad");
   }
 }
